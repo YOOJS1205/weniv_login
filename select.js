01b773e4592b2858a9b27ce29c797fc7b4f10877
YOOJS1205 <junsang8921@gmail.com>
2022-04-19 10:09:08 +0900

    이미지 경로 수정

--- select.js
+++ select.js
@@ -3,11 +3,11 @@ const OptionBox = document.querySelector('.options');
 
 // e.target vs e.currentTarget 정리
 SelectBox.addEventListener('click', (e) => {
-    if (e.currentTarget.childNodes[1].src == 'http://127.0.0.1:5500/images/icon-Triangle-down.png') {
-        e.currentTarget.childNodes[1].src = 'http://127.0.0.1:5500/images/icon-Triangle-up.png';
+    if (e.currentTarget.childNodes[1].src == './images/icon-Triangle-down.png') {
+        e.currentTarget.childNodes[1].src = './images/icon-Triangle-up.png';
     }
     else {
-        e.currentTarget.childNodes[1].src = 'http://127.0.0.1:5500/images/icon-Triangle-down.png'
+        e.currentTarget.childNodes[1].src = './images/icon-Triangle-down.png'
     }
     OptionBox.classList.toggle('hidden');
 })
